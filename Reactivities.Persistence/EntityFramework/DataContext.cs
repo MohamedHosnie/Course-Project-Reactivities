348ead444cc3bdb17b23a389c131d0a9e30d1cf0
MohamedHosnie <mohamed.hosnie1@gmail.com>
2021-04-19 22:29:32 +0200

    InitialCommit

---
+++ Reactivities.Persistence/EntityFramework/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reactivities.Domain.Activities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reactivities.Persistence.EntityFramework
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Activity> Activities { get; set; }
+	}
+}
