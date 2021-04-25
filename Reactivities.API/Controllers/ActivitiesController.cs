39fe84c86d177f9d16e0716ecef51788d51c4806
MohamedHosnie <mohamed.hosnie1@gmail.com>
2021-04-25 04:31:19 +0200

    Finished Section 4

--- Reactivities.API/Controllers/ActivitiesController.cs
+++ Reactivities.API/Controllers/ActivitiesController.cs
@@ -1,30 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Reactivities.Application.Activities;
 using Reactivities.Domain.Activities;
-using Reactivities.Persistence.EntityFramework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Reactivities.API.Controllers
 {
 	public class ActivitiesController : BaseApiController
 	{
-		private readonly DataContext _context;
-
-		public ActivitiesController(DataContext context) {
-			_context = context;
-		}
-
 		[HttpGet]
 		public async Task <ActionResult<List<Activity>>> GetAll() {
-			return await _context.Activities.ToListAsync();
+			return await Mediator.Send(new List.Query());
 		}
 
 		[HttpGet("{id}")]
-		public async Task<Activity> Get(Guid id) {
-			return await _context.Activities.FindAsync(id);
+		public async Task<ActionResult<Activity>> Get(Guid id) {
+			return await Mediator.Send(new Details.Query { Id = id });
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(Activity activity) {
+			return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> Edit(Guid id, Activity activity) {
+			activity.Id = id;
+			return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete(Guid id) {
+			return Ok(await Mediator.Send(new Delete.Command { Id = id }));
 		}
 	}
 }
---
+++ Reactivities.Application/Core/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Reactivities.Domain.Activities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reactivities.Application.Core
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<Activity, Activity>();
+		}
+	}
+}
